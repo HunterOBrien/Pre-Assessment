66d39e85532a43063d2aa785d26df93a25bb1e37
HunterOBrien <obrienh2@middleton.school.nz>
2023-03-28 12:15:14 +1300

    added comments and exit menu to v2 of welcome

--- welcome_v2.py
+++ welcome_v2.py
@@ -2,8 +2,10 @@ import easygui
 import os
 import PIL
 import time
+
 # Right click on import PIl and install it to your device
 
+# Dictionary that stores the different combos available
 combos = {
     "VALUE":
         {"Beef burger": "5.69",
@@ -24,10 +26,17 @@ combos = {
 
 
 def welcome():
+    # Finds where the image is in the user's files
     logo_image = os.environ['USERPROFILE'] + "\\Downloads\\burger logo.jpg"
+    # Welcome and logo
     easygui.msgbox("WELCOME TO TRUMP'S BURGER TRUCK!", "Trump's burgers", image=logo_image)
+    # Displays the different options for the combos
     menu_choices = easygui.buttonbox("Pick one of the below options for the Burger Menu", "Burger Menu Options",
                                      choices=("View Menu", "Add Combo", "Remove Combo", "Change Combo", "Exit Menu"))
+    # If the user wants to exit the combos menu it waits 0.75sec then goes to main menu
+    if menu_choices == "Exit Menu":
+        time.sleep(0.75)
+        welcome()
 
 
 welcome()
